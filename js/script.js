d5b65cfb058897b771f15487c9c6d0d391ab48b1
Night Hunter <deveshthakur108@gmail.com>
2024-09-01 16:56:02 +0530

    update sidebar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function login(){
-        location.href="login_signup/login.html";
+function login() {
+        location.href = "login_signup/login.html";
+}
+function toggleSidebar() {
+        const sidebar = document.getElementById('navbar-right');
+        navbar-right.classList.toggle('active');
 }
