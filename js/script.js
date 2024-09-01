45f82e9f613782ae1c924dc9d7642e12f798145a
Aaieeee <ifhybilyou@gmail.com>
2024-09-01 12:08:45 -0700

    ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 function login() {
-        location.href = "login_signup/login.html";
+  location.href = "login_signup/login.html";
 }
 function toggleSidebar() {
-        const sidebar = document.getElementByClass('navbar-right');
-        sidebar.classList.toggle('active');
+  const sidebar = document.getElementsByClassName("navbar-right");
+  sidebar.classList.toggle("active");
 }
+let deveshBhai = true;
+document.querySelector("#Layer_1").addEventListener("click", function () {
+  if (deveshBhai) {
+    document.querySelector(".navbar-right").style.opacity = 1;
+    deveshBhai = false;
+  } else {
+    document.querySelector(".navbar-right").style.opacity = 0;
+
+    deveshBhai = true;
+  }
+});
