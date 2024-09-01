ad80a7dc93020d966901959dad33db02515f3d95
Night Hunter <deveshthakur108@gmail.com>
2024-09-01 22:14:01 +0530

    minor update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ function login() {
         location.href = "login_signup/login.html";
 }
 function toggleSidebar() {
-        const sidebar = document.getElementById('navbar-right');
-        navbar-right.classList.toggle('active');
+        const sidebar = document.getElementByClass('navbar-right');
+        sidebar.classList.toggle('active');
 }
